67dffc435b9a531c2043f7cb603f6e3ddd6dc155
Ajith Bobby <ajithbobbyabk@gmail.com>
2022-12-09 20:53:54 +0530

    Update prime_numbers.py
    Python program to find all prime numbers in a range

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,10 +1,10 @@
-from timeit import default_timer as timer
-start = timer()
-def prime_numbers(n):
-    out = list()
-    for x in range(1, n+1):
-        if all(x % i != 0 for i in range (2, int(x**.5)+1)):
-            out.append(x)
-    return out
-end = timer()
-print (end -start)
+import math
+start = int(input('Enter the start number : '))
+end = int(input('Enter the end number : '))
+for num in range(start, end+1):
+    if num > 1:
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if(num % i) == 0:
+                break;
+        else:
+            print(num)
